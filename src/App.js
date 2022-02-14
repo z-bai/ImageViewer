925c71cb3201f52e36407b3572bc3b46293a6e4e
Zhengquan Bai <jerryqueue@gmail.com>
2022-02-14 18:09:29 +0800

    content editable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import ImageViewer from "./ImageViewer";
 import './App.css';
+import React from 'react';
+import ContentEditable from 'react-contenteditable';
 
-function App() {
-  return (
-    <div className="App">
-      <ImageViewer />
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            html: ``
+        };
+    }
+
+    withTagsStriped = (str) => str.replace(/(<([^>]+)>)/gi, "");
+    wrapWithTags = (str) => str.replace(/(\$.+?\$)/g, '<span class="highlighted">$1</span>');
+
+    handleChange = (evt) => {
+        const value = evt.target.value;
+        const pureText = this.withTagsStriped(value);
+        console.log('pureText', pureText);
+        this.setState({
+            html: this.wrapWithTags(pureText)
+        });
+    };
+
+    componentDidMount() {
+        document.onselectionchange = function() {
+            console.log('selection change');
+        }
+    }
+
+    render = () => {
+        return (
+            <div className="App">
+                <ContentEditable
+                    html={this.state.html}
+                    onChange={this.handleChange}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
